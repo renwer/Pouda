f0d234eadfdaed0d0b51c0dc19bfa9ce2e712497
Ihor Mykytenko <mykytenko.igor99@gmail.com>
2018-04-14 14:27:29 +0200

    Fix the scrolling issue, add primary color pallet
    The chat view now scrolls itself down to the latest messages.
    Better colors for panes and messages added

--- src/com/renwer/strixMain/Controller.java
+++ src/com/renwer/strixMain/Controller.java
@@ -2,6 +2,7 @@ package com.renwer.strixMain;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 
@@ -13,21 +14,27 @@ public class Controller {
     private TextField messageField;
     @FXML
     private VBox messages;
+    @FXML
+    private VBox activeUsers;
+    @FXML
+    private ScrollPane messagePane;
+
 
 
     public void onSendButtonClicked() {
         if (!messageField.getText().equals("")) {
             Label message = new Label(messageField.getText());
-            message.setStyle("-fx-background-color: cyan; -fx-padding: 10px; -fx-background-insets: 5px;");
-            message.setOpacity(0.55);
+            message.setStyle("-fx-background-color: #d7f9c5; -fx-padding: 10px; -fx-background-insets: 5px;");
             messageField.setText("");
             messages.getChildren().add(message);
-            messageField.requestFocus();
         }
+        messages.heightProperty().addListener(observable -> messagePane.setVvalue(1D));
+        messageField.requestFocus();
     }
 
-    public void onMessageToServer() {
+    public void onUserLogin(String username) {
 
     }
+
 
 }
