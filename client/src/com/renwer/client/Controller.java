13319a76640f3d197425795f286fa4fbc099ed76
Alexander Strelchenko <furion1gg@gmail.com>
2018-04-16 23:54:37 +0300

    Added client code for connection

--- client/src/com/renwer/client/Controller.java
+++ client/src/com/renwer/client/Controller.java
@@ -1,5 +1,8 @@
 package com.renwer.client;
 
+import com.renwer.networkelements.Connection;
+import com.renwer.networkelements.ConnectionListener;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -8,13 +11,14 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 
-public class Controller {
+public class Controller implements ConnectionListener{
 
     public static final String IP_ADDRESS = "127.0.0.1";
     public static final int PORT = 8189;
     
-
+    private Connection connection;
 
     @FXML
     private TextField messageField;
@@ -27,6 +31,38 @@ public class Controller {
 
 
     @FXML
+    private void initialize(){
+        try {
+            this.connection = new Connection(this, IP_ADDRESS, PORT);
+        }catch(IOException e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void onConnectionReady(Connection connection) {
+        addMessageToChat("Connected");
+    }
+
+    @Override
+    public void onStringReceived(Connection connection, String string) {
+        System.out.println("Received message: " + string);
+        addMessageToChat(string);
+    }
+
+    @Override
+    public void onAbortConnection(Connection connection) {
+        addMessageToChat("Disconnected");
+    }
+
+    @Override
+    public void onException(Connection connection, Exception e) {
+        addMessageToChat("Got some problem: " + e.getMessage());
+    }
+
+    @FXML
     private void handleEnterButtonPressed(KeyEvent ke){
         if(ke.getCode() == KeyCode.ENTER){
             onSendButtonClicked();
@@ -35,14 +71,26 @@ public class Controller {
 
     public void onSendButtonClicked() {
         if (!messageField.getText().equals("")) {
-            Label message = new Label(messageField.getText());
-            message.setStyle("-fx-background-color: #d7f9c5; -fx-padding: 10px; -fx-background-insets: 5px; -fx-font-size: 15px; -fx-background-radius: 8px");
+            String messageText = messageField.getText();
+            connection.sendString(messageText);
             messageField.setText("");
-            messages.getChildren().add(message);
         }
-        messages.heightProperty().addListener(observable -> messagePane.setVvalue(1D));
-        messageField.requestFocus();
     }
 
+    private void addMessageToChat(String message){
+        if(message != null && message.length() != 0){
+            Label messageLabel = new Label(message);
+            messageLabel.setStyle("-fx-background-color: #d7f9c5; -fx-padding: 10px; -fx-background-insets: 5px; -fx-font-size: 15px; -fx-background-radius: 8px");
+
+            //This is important. Doesnt work without this.
+            Platform.runLater(() ->
+                    messages.getChildren().add(messageLabel)
+            );
+
+            messages.heightProperty().addListener(observable -> messagePane.setVvalue(1D));
+            messageField.requestFocus();
+        }
+
+    }
 
 }
