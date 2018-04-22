167ed0e6cd2185aa08c377f20428d2707f339c27
Alexander Strelchenko <furion1gg@gmail.com>
2018-04-22 23:11:22 +0300

    A little refactor

--- server/src/com/renwer/server/Server.java
+++ server/src/com/renwer/server/Server.java
@@ -9,15 +9,19 @@ import java.util.StringJoiner;
 
 public class Server implements ConnectionListener {
 
+    private final static String DEFAULT_SERVER_NAME = "Simple Chat Server";
+    private String serverName;
+
     public static void main(String[] args) {
         new Server();
     }
 
     /** All connections */
-    public static ArrayList<Connection> connections = new ArrayList<>();
+    private static ArrayList<Connection> connections = new ArrayList<>();
 
-    private Server() {
+    private Server(String serverName){
         System.out.println("Server up and running");
+        this.serverName = serverName;
 
         try (ServerSocket serverSocket = new ServerSocket(8189)) {
             while (true) {
@@ -33,6 +37,10 @@ public class Server implements ConnectionListener {
         }
     }
 
+    private Server() {
+        this(DEFAULT_SERVER_NAME);
+    }
+
     @Override
     public synchronized void onConnectionReady(Connection connection) {
         connections.add(connection);
@@ -42,21 +50,7 @@ public class Server implements ConnectionListener {
     public synchronized void onStringReceived(Connection connection, String string) {
         if(string.startsWith("TYPE:REGISTER")){
             String userName = string.substring(string.indexOf(':', string.indexOf("USERNAME:")) + 1);
-
-            if (!isUniqueUserName(userName)) {
-                userName = generateUniqueUserName(userName);
-            }
-
-            connection.setUserName(userName);
-            connection.sendString("TYPE:REGISTER USERNAME:" + userName);
-
-            StringJoiner allUsersStringJoiner = new StringJoiner(",");
-            for(Connection c : connections){
-                allUsersStringJoiner.add(c.getUserName());
-            }
-
-            sendToAllConnections("TYPE:USER_LIST USERS:" + allUsersStringJoiner.toString());
-            sendToAllConnections(userName + " joined the chat room");
+            registerNewUser(connection, userName);
         }else {
             sendToAllConnections(connection.getUserName() + ": " + string);
         }
@@ -72,6 +66,23 @@ public class Server implements ConnectionListener {
     public synchronized void onException(Connection connection, Exception e) {
         connection.abortConnection();
         System.out.println("Exception caught " + e);
+    }
+
+    private synchronized void registerNewUser(Connection connection, String userName){
+        if (!isUniqueUserName(userName)) {
+            userName = generateUniqueUserName(userName);
+        }
+
+        connection.setUserName(userName);
+        connection.sendString("TYPE:REGISTER SERVERNAME:" + serverName + " USERNAME:" + userName);
+
+        StringJoiner allUsersStringJoiner = new StringJoiner(",");
+        for(Connection c : connections){
+            allUsersStringJoiner.add(c.getUserName());
+        }
+
+        sendToAllConnections("TYPE:USER_LIST USERS:" + allUsersStringJoiner.toString());
+        sendToAllConnections(userName + " joined the chat room");
     }
 
     private synchronized void sendToAllConnections(String message) {
